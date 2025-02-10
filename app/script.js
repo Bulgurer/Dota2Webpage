54701a8e53531ede06fbf5be94e9100540bb7e79
Boris <nedevbboris@gmail.com>
2025-02-10 05:13:42 +0000

    Made a function that returns a promise that contains the hero data so that it can be more easily accessable to other functions that need it.

--- app/script.js
+++ app/script.js
@@ -3,16 +3,22 @@ const heroImage = document.querySelector('#hero-image');
 buttonNewHero.onclick = pickNewHero;
 
 function pickNewHero() {
-    changeHeroImage();
-    changeHeroName()
+    let newHero = getNewHero();
+    console.log(newHero);
+    changeHeroImage(newHero);
+    //changeHeroName()
 }
 
-function changeHeroImage() {
-    fetch('https://api.opendota.com/api/heroes')
-        .then(response => response.json())
-        .then(data => {
-            const randomHero = data[Math.floor(Math.random() * data.length)];
-            heroImage.src = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${randomHero.name.replace('npc_dota_hero_', '')}.png`;
-            console.log("🚀 ~ pickNewHero ~ data:", data);
-        });
+function getNewHero() {
+    return fetch('https://api.opendota.com/api/heroes')
+                    .then(response => response.json())
+                    .then(data => {
+                        return data[Math.floor(Math.random() * data.length)]});
+}
+
+function changeHeroImage(randomHero) {
+    randomHero.then(data => {
+        console.log(data);
+        heroImage.src = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${data.name.replace('npc_dota_hero_', '')}.png`;
+    });
 }
