66b3899f585b922bd64437113d528a2ff3a18dc1
Boris <nedevbboris@gmail.com>
2025-02-10 04:28:37 +0000

    Hero images now change when clickign new hero button

--- app/script.js
+++ app/script.js
@@ -3,5 +3,16 @@ const heroImage = document.querySelector('#hero-image');
 buttonNewHero.onclick = pickNewHero;
 
 function pickNewHero() {
-    heroImage.src = "https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/axe.png";
+    changeHeroImage();
+    changeHeroName()
+}
+
+function changeHeroImage() {
+    fetch('https://api.opendota.com/api/heroes')
+        .then(response => response.json())
+        .then(data => {
+            const randomHero = data[Math.floor(Math.random() * data.length)];
+            heroImage.src = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${randomHero.name.replace('npc_dota_hero_', '')}.png`;
+            console.log("🚀 ~ pickNewHero ~ data:", data);
+        });
 }
