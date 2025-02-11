263e5a0ce54fc9ed1f570e90ba0955e6336041d7
Boris <nedevbboris@gmail.com>
2025-02-11 01:01:27 +0000

    Changed the code to request info from Stratz because it is more complete. Also successfully gave all heroes their descriptions.

--- app/script.js
+++ app/script.js
@@ -1,30 +1,62 @@
+
+const STRATZ_API_KEY = CONFIG.STRATZ_API_KEY; // Temporary solution to access the API key from config.js
+
 const buttonNewHero = document.querySelector('#button-new-hero');
 const heroImage = document.querySelector('#hero-image');
 const heroName = document.querySelector('#hero-name');
+const heroDescription = document.querySelector('#hero-description');
+
 buttonNewHero.onclick = pickNewHero;
 
+const stratz = 'https://api.stratz.com/api/v1/Hero';
+const stratzHeaders = new Headers({
+    'Content-Type': 'application/json',
+    Authorization: 'Bearer ' + STRATZ_API_KEY,
+    UserAgent: 'STRATZ_API'
+});
+
+const openDota = 'https://api.opendota.com/api/heroes';
+
+const cloudflare = 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/';
+
+
 function pickNewHero() {
-    let newHero = getNewHero();
+    let newHero = getNewHeroStratz();
     console.log(newHero);
     changeHeroImage(newHero);
     changeHeroName(newHero);
+    changeHeroDescription(newHero);
 }
 
-function getNewHero() {
-    return fetch('https://api.opendota.com/api/heroes')
+function getNewHeroStratz() {
+    return fetch(stratz, {headers: stratzHeaders})
                     .then(response => response.json())
-                    .then(data => {
-                        return data[Math.floor(Math.random() * data.length)]});
+                    .then((data) => {
+                        keys = Object.keys(data);
+                        return data[keys[Math.floor(Math.random() * (keys.length - 2)) + 1]]}) // +1 to avoid the first key which is a null hero and -2 to avoid the last key which is a dummy hero.
+                    .catch(error => console.log(error));
+                }
+
+function getNewHero() {
+    return fetch(openDota)
+                    .then(response => response.json())
+                    .then(data => data[Math.floor(Math.random() * data.length)]);
 }
 
 function changeHeroImage(newHero) {
     newHero.then(data => {
-        heroImage.src = `https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/${data.name.replace('npc_dota_hero_', '')}.png`;
+        heroImage.src = cloudflare + data.shortName + '.png';
     });
 }
 
 function changeHeroName(newHero) {
     newHero.then(data => {
-        heroName.textContent = data.localized_name;
-    })
+        heroName.textContent = data.displayName;
+    });
+}
+
+function changeHeroDescription(newHero) {
+    newHero.then(data => {
+        heroDescription.innerHTML = data.language.hype.replace(/<b>|<\/b>/g, '');
+    });
 }
