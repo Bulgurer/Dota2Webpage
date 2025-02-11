d34c603eab12d0d1815a89bd331c19480a5d592f
Boris <nedevbboris@gmail.com>
2025-02-11 20:57:42 +0000

    Hero backgrounds now change accordingly

--- app/script.js
+++ app/script.js
@@ -5,6 +5,7 @@ const buttonNewHero = document.querySelector('#button-new-hero');
 const heroImage = document.querySelector('#hero-image');
 const heroName = document.querySelector('#hero-name');
 const heroDescription = document.querySelector('#hero-description');
+const heroSection = document.querySelector('#hero-section');
 
 buttonNewHero.onclick = pickNewHero;
 
@@ -15,8 +16,6 @@ const stratzHeaders = new Headers({
     UserAgent: 'STRATZ_API'
 });
 
-const openDota = 'https://api.opendota.com/api/heroes';
-
 const cloudflare = 'https://cdn.cloudflare.steamstatic.com/apps/dota2/images/dota_react/heroes/';
 
 
@@ -26,6 +25,7 @@ function pickNewHero() {
     changeHeroImage(newHero);
     changeHeroName(newHero);
     changeHeroDescription(newHero);
+    changeHeroSectionColor(newHero);
 }
 
 function getNewHeroStratz() {
@@ -36,12 +36,6 @@ function getNewHeroStratz() {
                         return data[keys[Math.floor(Math.random() * (keys.length - 2)) + 1]]}) // +1 to avoid the first key which is a null hero and -2 to avoid the last key which is a dummy hero.
                     .catch(error => console.log(error));
                 }
-
-function getNewHero() {
-    return fetch(openDota)
-                    .then(response => response.json())
-                    .then(data => data[Math.floor(Math.random() * data.length)]);
-}
 
 function changeHeroImage(newHero) {
     newHero.then(data => {
@@ -59,4 +53,60 @@ function changeHeroDescription(newHero) {
     newHero.then(data => {
         heroDescription.innerHTML = data.language.hype.replace(/<b>|<\/b>/g, '');
     });
+}
+
+function changeHeroSectionColor(newHero) {
+    let color;
+    let orientation = '180deg';
+    let green = '#008000';
+    let blue = '#1E90FF';
+    let red = '#7C0A02';
+    newHero.then(data => {
+        switch(data.stat.heroPrimaryAttribute) {
+            case 0: // Strength
+                color = red;
+                break;
+            case 1: // Universal
+                color = green;
+                break;
+            case 2: // Intelligence
+                color = blue;
+                break;
+            case 3: // Agility
+                heroSection.style.backgroundImage = getCssValuePrefix() + 'linear-gradient('
+                + orientation + ', ' + red + ', ' + blue + ', ' + green + ')';
+                break;
+            
+        }
+        heroSection.style.background = color;
+        
+        // Setting the gradient with the proper prefix
+        
+    });
+}
+
+function getCssValuePrefix()
+{
+    var rtrnVal = '';//default to standard syntax
+    var prefixes = ['-o-', '-ms-', '-moz-', '-webkit-'];
+
+    // Create a temporary DOM object for testing
+    var dom = document.createElement('div');
+
+    for (var i = 0; i < prefixes.length; i++)
+    {
+        // Attempt to set the style
+        dom.style.background = prefixes[i] + 'linear-gradient(#000000, #ffffff)';
+
+        // Detect if the style was successfully set
+        if (dom.style.background)
+        {
+            rtrnVal = prefixes[i];
+        }
+    }
+
+    dom = null;
+    delete dom;
+
+    return rtrnVal;
 }
