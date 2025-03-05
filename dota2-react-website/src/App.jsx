3734a8c562e6186f3aacfb7f0e94c4cba2bcae6b
Boris <nedevbboris@gmail.com>
2025-03-05 20:43:26 +0000

    Improved API calls so that all Hero ids are fetched on first render so that we only have to make calls for specific heroes

--- dota2-react-website/src/App.jsx
+++ dota2-react-website/src/App.jsx
@@ -3,9 +3,41 @@ import "./App.css";
 import HeroSection from "./components/HeroSection.jsx";
 
 import env from "react-dotenv";
+import SkillBuild from "./components/SkillBuild.jsx";
 
 export default function App() {
+  const [allHeroIds, setAllHeroIds] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.stratz.com/graphql", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + window.env.REACT_APP_STRATZ_API_KEY,
+        UserAgent: "STRATZ_API",
+      },
+      body: JSON.stringify({
+        query: `{
+          constants{
+            heroes{
+              id
+            }
+          }
+        }`,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        let heroes = data.data.constants.heroes;
+        let heroIds = heroes.map((hero) => hero.id);
+        console.log(heroIds);
+        setAllHeroIds(heroIds); // for some reason the hero ids jump around in value, array.length - 1 is NOT the same as the highest ID
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
   const [currentHero, setCurrentHero] = useState({
+    id: null, // Default hero id
     image: "https://img.icons8.com/color/512/dota.png", // Default hero image
     name: "Dota 2", // Default hero name
     description: "Click the 'New hero' button to get started!", // Default hero description
@@ -14,8 +46,10 @@ export default function App() {
   const [history, setHistory] = useState([currentHero]);
 
   function handleClick() {
-    console.log("New hero button clicked");
-    let newHeroPromise = getNewHeroStratz();
+    let newHeroId =
+      allHeroIds[Math.floor(Math.random() * (allHeroIds.length - 1))];
+    console.log("-".repeat(100));
+    let newHeroPromise = getNewHeroStratz(newHeroId);
     newHeroPromise.then((data) => {
       let newImage =
         window.env.REACT_APP_HERO_IMAGES_URL + data.shortName + ".png"; // New hero image
@@ -44,11 +78,15 @@ export default function App() {
       <div id="hero-sections">
         <HeroSection currentHero={currentHero} onButtonClick={handleClick} />
       </div>
+      <div id="build" className="centeredHorizontal">
+        <SkillBuild />
+      </div>
     </main>
   );
 }
 
-function getNewHeroStratz() {
+function getNewHeroStratz(newHeroId) {
+  console.log("The new Hero ID is: " + newHeroId);
   return fetch("https://api.stratz.com/graphql", {
     method: "POST",
     headers: {
@@ -59,7 +97,7 @@ function getNewHeroStratz() {
     body: JSON.stringify({
       query: `{
         constants{
-          heroes{
+          hero(id:${newHeroId}){
             displayName
             shortName
             language{
@@ -75,8 +113,8 @@ function getNewHeroStratz() {
   })
     .then((response) => response.json())
     .then((data) => {
-      let heroes = data.data.constants.heroes;
-      return heroes[Math.floor(Math.random() * (heroes.length - 1))];
+      console.log(data.data.constants.hero);
+      return data.data.constants.hero;
     })
     .catch((error) => console.log(error));
 }
